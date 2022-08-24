1f2daddbb0ff15a49bd042357804bd1729e6fd2a
David Castro <davidcastrosi@hotmail.com>
2022-08-24 09:13:03 -0600

    Mejora en el mensaje del correo

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def send_mail(name, email, subject, message):
         connection.sendmail(
             from_addr=EMAIL,
             to_addrs="davidcastrosi@hotmail.com",
-            msg="Subject:Hello\n\nThis is the body of my email."
+            msg=f"Subject:{subject}\n\nMessage: {message}\nName: {name} Email: {email}"
         )
 
 @app.route("/", methods=['GET', 'POST'])
