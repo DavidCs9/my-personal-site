49f14e1c136952c5597aac552f48970c57ade65c
David Castro <davidcastrosi@hotmail.com>
2022-12-06 18:45:12 -0600

    quite la seccion de email del archivo application.py

--- application.py
+++ application.py
@@ -4,13 +4,13 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
-#EMAIL = "decsiqueiros198@gmail.com"
-#PSW = os.environ['PSW']
+# EMAIL = "decsiqueiros198@gmail.com"
+# PSW = os.environ['PSW']
 
 application = app = Flask(__name__)
 
 
-#def send_mail(name, email, subject, message):
+# def send_mail(name, email, subject, message):
 #    with smtplib.SMTP("smtp.gmail.com") as connection:
 #        connection.starttls()
 #        connection.login(user=EMAIL, password=PSW)
@@ -23,20 +23,20 @@ application = app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    name = request.form.get('name')
-    email = request.form.get('email')
-    subject = request.form.get('subject')
-    message = request.form.get('message')
+    # name = request.form.get('name')
+    # email = request.form.get('email')
+    # subject = request.form.get('subject')
+    # message = request.form.get('message')
 
-    #if request.method == 'POST':
+    # if request.method == 'POST':
     #    send_mail(name, email, subject, message)
     #    return render_template("email.html")
 
-    #return render_template("index.html")
+    return render_template("index.html")
 
 
-#@app.route("/email", methods=['GET'])
-#def email():
+# @app.route("/email", methods=['GET'])
+# def email():
 #    return render_template("email.html")
 
 
