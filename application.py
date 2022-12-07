c062f4deae23349d8c639a2e3a286f0639fe6b8c
David Castro <davidcastrosi@hotmail.com>
2022-12-06 18:44:08 -0600

    quite la seccion de email del archivo application.py

--- application.py
+++ application.py
@@ -4,21 +4,21 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
-EMAIL = "decsiqueiros198@gmail.com"
-PSW = os.environ['PSW']
+#EMAIL = "decsiqueiros198@gmail.com"
+#PSW = os.environ['PSW']
 
 application = app = Flask(__name__)
 
 
-def send_mail(name, email, subject, message):
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=EMAIL, password=PSW)
-        connection.sendmail(
-            from_addr=EMAIL,
-            to_addrs="davidcastrosi@hotmail.com",
-            msg=f"Subject:{subject}\n\nMessage: {message}\nName: {name} Email: {email}"
-        )
+#def send_mail(name, email, subject, message):
+#    with smtplib.SMTP("smtp.gmail.com") as connection:
+#        connection.starttls()
+#        connection.login(user=EMAIL, password=PSW)
+#        connection.sendmail(
+#            from_addr=EMAIL,
+#            to_addrs="davidcastrosi@hotmail.com",
+#            msg=f"Subject:{subject}\n\nMessage: {message}\nName: {name} Email: {email}"
+#        )
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -28,16 +28,16 @@ def home():
     subject = request.form.get('subject')
     message = request.form.get('message')
 
-    if request.method == 'POST':
-        send_mail(name, email, subject, message)
-        return render_template("email.html")
+    #if request.method == 'POST':
+    #    send_mail(name, email, subject, message)
+    #    return render_template("email.html")
 
-    return render_template("index.html")
+    #return render_template("index.html")
 
 
-@app.route("/email", methods=['GET'])
-def email():
-    return render_template("email.html")
+#@app.route("/email", methods=['GET'])
+#def email():
+#    return render_template("email.html")
 
 
 if __name__ == "__main__":
